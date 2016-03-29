10bb19af2f738108b5236194547421053f62413e
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2016-03-28 22:15:09 -0300

    Trying to find a way to workaroung <base> at index.html

--- src/main/webapp/app/app.router.js
+++ src/main/webapp/app/app.router.js
@@ -1,22 +1,20 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	angular.module('app').config([ '$routeProvider', '$locationProvider', routing ]);
+    angular.module('app').config([ '$routeProvider', '$locationProvider', routing ]);
 
-	function routing($routeProvider, $locationProvider) {
-		$routeProvider.when('/greet', {
-			templateUrl : 'app/components/greet/greet.html',
-			controller : 'GreetingCtrl'
-		}).when('/config', {
-			templateUrl : 'app/components/config/config.html',
-			controller : 'ConfigCtrl'
-		}).otherwise({
-			redirectTo : '/greet'
-		});
-		
-		$locationProvider.html5Mode($locationProvider.html5Mode({
-			  enabled: false
-			}));
-	}
+    function routing($routeProvider, $locationProvider) {
+        $routeProvider.when('/greet', {
+            templateUrl : 'app/components/greet/greet.html'
+        }).when('/config', {
+            templateUrl : 'app/components/config/config.html'
+        }).otherwise({
+            redirectTo : '/greet'
+        });
+
+        $locationProvider.html5Mode($locationProvider.html5Mode({
+            enabled : true
+        }));
+    }
 
 })();
